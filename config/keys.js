5a30e5ef659618ecc911e061e0908e554683e49b
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-04 10:21:58 +0530

    database connection key

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://un4iolhc8dj0nbi3dzh8:K9sqAy55GkfGnOuHU9fd@bpxpyz5wazcp3kz-mongodb.services.clever-cloud.com:27017/bpxpyz5wazcp3kz",
+  secretOrKey: "rentaLinks"
+};

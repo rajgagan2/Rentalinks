1f3e9c143394e9ec0fe5b7469007a5a9d4feff1e
rajgagan2 <levigagan7@gmail.com>
2019-08-24 11:39:51 +0530

    Added cost calculation and display pickup drop date and time

--- routes/products/index.js
+++ routes/products/index.js
@@ -1,17 +1,39 @@
 const router    = require("express").Router(),
-        Car     = require("../../models/cars");
+        Car     = require("../../models/cars"),
+        moment  = require("moment");
 
-router.get("/products",function(req,res){
+router.post("/products",function(req,res){
+
+    var hours = calBookingHours(req.body.pickDate,req.body.pickTime,req.body.dropDate,req.body.dropTime);
+    var data = {
+        bookingHours: hours,
+        pickDate: moment(req.body.pickDate).format("Do MMM"),
+        pickTime: moment(req.body.pickTime, "HH:mm:ss").format("hh[:]mm a"),
+        dropDate: moment(req.body.dropDate).format("Do MMM"),
+        dropTime: moment(req.body.dropTime, "HH:mm:ss").format("hh[:]mm a")
+    };
+    console.log(data.bookingHours);
     Car.find().then(cars =>{
         if (!cars) {
             return res.status(404).json({ noCarsFound: "No cars found" });
           }
         // Get cars
         if (cars) {
-            return res.render("./products/products", { cars: cars });
+            return res.render("./products/products", { cars: cars, data:data });
             }
     });
 });
+
+function calBookingHours(pd,pt,dd,dt){
+
+    const pickDateTime = moment(`${pd} ${pt}`, 'YYYY-MM-DD HH:mm:ss');
+    const dropDateTime = moment(`${dd} ${dt}`, 'YYYY-MM-DD HH:mm:ss');
+    console.log(pickDateTime,dropDateTime);
+
+    // console.log(dropDateTime.diff(pickDateTime,"hours"));
+   
+    return dropDateTime.diff(pickDateTime,"hours");
+}
 
 module.exports = router;
 
